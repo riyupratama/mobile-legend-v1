f19843e8a8440e2a2b208281048cbd8aace16749
Riyu R Pratama <38023284+riyupratama@users.noreply.github.com>
2023-12-04 14:12:25 +0700

    Add file via upload
    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+function myToggle(){
+    var fullPartner =  document.querySelector(".full-partner");
+
+    if(fullPartner.style.display === "none"){
+        fullPartner.style.display = "block";
+    }else{
+        fullPartner.style.display = "none"
+    }
+}
